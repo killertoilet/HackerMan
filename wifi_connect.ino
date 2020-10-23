a1ef57fabd6bea3d7c7a3608720652d78c1d9efe
killertoilet <52977486+killertoilet@users.noreply.github.com>
2020-10-23 16:53:38 -0700

    Add files via upload
    Connect that meaty esp8266 to wifi

---
+++ wifi_connect.ino
@@ -0,0 +1,27 @@
+
+#include "ESP8266WiFi.h"
+
+const char* ssid = "I Can Haz Wireless?"; //Enter SSID
+const char* password = "youcanthaveit"; //Enter Password
+
+void setup(void)
+{ 
+  Serial.begin(115200);
+  // Connect to WiFi
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) 
+  {
+     delay(500);
+     Serial.print("*");
+  }
+  
+  Serial.println("");
+  Serial.println("WiFi connection Successful");
+  Serial.print("The IP Address of ESP8266 Module is: ");
+  Serial.print(WiFi.localIP());// Print the IP address
+}
+
+void loop() 
+{
+  // EMPTY
+}
